6e4a59f32f7523813cb4c5ecfbec74265b33f1b7
Francielly Cardoso <ciellycardoso@gmail.com>
2015-06-11 10:55:38 -0300

    Starting implementation

---
+++ src/ppmc.cpp
@@ -0,0 +1,12 @@
+#include "ppmc.hpp"
+
+PPMC::~PPMC(void){}
+
+/* create Tree */ 
+PPMC::PPMC(void){
+
+	Node *root = new Node("");
+	Node *newESC = new Node("ESC");
+
+	root->setChildren("ESC", newESC); 
+}
